e2337459098584699f36f0a39c460c5ff34dc44e
40ushek <40ushek@gmail.com>
2026-04-10 19:59:08 +0100

    Build mark query results from marks view context

--- src/TeklaMcpServer.Api/Drawing/Marks/TeklaDrawingMarkApi.Query.cs
+++ src/TeklaMcpServer.Api/Drawing/Marks/TeklaDrawingMarkApi.Query.cs
@@ -27,10 +27,13 @@ public sealed partial class TeklaDrawingMarkApi
 
             var marks = new List<DrawingMarkInfo>();
             var seenIds = new HashSet<int>();
+            var contextBuilder = new MarksViewContextBuilder();
 
             foreach (var view in viewsToQuery)
             {
                 var vid = view.GetIdentifier().ID;
+                var viewContext = contextBuilder.Build(view, _model);
+                var contextsById = viewContext.Marks.ToDictionary(item => item.MarkId);
                 var markObjects = view.GetAllObjects(typeof(Mark));
 
                 while (markObjects.MoveNext())
@@ -42,15 +45,15 @@ public sealed partial class TeklaDrawingMarkApi
                     if (!seenIds.Add(markId))
                         continue;
 
+                    contextsById.TryGetValue(markId, out var markContext);
                     var bbox = mark.GetAxisAlignedBoundingBox();
                     var obb = mark.GetObjectAlignedBoundingBox();
-                    var geometry = MarkGeometryHelper.Build(mark, _model, vid);
                     var ins = mark.InsertionPoint;
                     var leaderLinePlacing = mark.Placing as LeaderLinePlacing;
                     var info = new DrawingMarkInfo
                     {
                         Id = markId,
-                        ViewId = vid,
+                        ViewId = markContext?.ViewId ?? vid,
                         InsertionX = Math.Round(ins.X, 1),
                         InsertionY = Math.Round(ins.Y, 1),
                         BboxMinX = Math.Round(bbox.MinPoint.X, 1),
@@ -59,60 +62,28 @@ public sealed partial class TeklaDrawingMarkApi
                         BboxMaxY = Math.Round(bbox.MaxPoint.Y, 1),
                         CenterX = Math.Round((bbox.MinPoint.X + bbox.MaxPoint.X) / 2.0, 2),
                         CenterY = Math.Round((bbox.MinPoint.Y + bbox.MaxPoint.Y) / 2.0, 2),
-                        PlacingType = mark.Placing?.GetType().Name ?? "null",
-                        PlacingX = leaderLinePlacing != null ? Math.Round(leaderLinePlacing.StartPoint.X, 2) : 0,
-                        PlacingY = leaderLinePlacing != null ? Math.Round(leaderLinePlacing.StartPoint.Y, 2) : 0,
+                        PlacingType = markContext?.PlacingType ?? mark.Placing?.GetType().Name ?? "null",
+                        PlacingX = markContext?.HasLeaderLine == true
+                            ? Math.Round(markContext.Anchor?.X ?? 0.0, 2)
+                            : leaderLinePlacing != null ? Math.Round(leaderLinePlacing.StartPoint.X, 2) : 0,
+                        PlacingY = markContext?.HasLeaderLine == true
+                            ? Math.Round(markContext.Anchor?.Y ?? 0.0, 2)
+                            : leaderLinePlacing != null ? Math.Round(leaderLinePlacing.StartPoint.Y, 2) : 0,
                         Angle = Math.Round(mark.Attributes.Angle, 2),
-                        RotationAngle = Math.Round(mark.Attributes.RotationAngle, 2),
-                        TextAlignment = mark.Attributes.TextAlignment.ToString(),
-                        ObjectAlignedBoundingBox = new MarkObjectAlignedBoundingBoxInfo
-                        {
-                            Width = Math.Round(obb.Width, 2),
-                            Height = Math.Round(obb.Height, 2),
-                            AngleToAxis = Math.Round(obb.AngleToAxis, 2),
-                            CenterX = Math.Round((obb.MinPoint.X + obb.MaxPoint.X) / 2.0, 2),
-                            CenterY = Math.Round((obb.MinPoint.Y + obb.MaxPoint.Y) / 2.0, 2),
-                            MinX = Math.Round(obb.MinPoint.X, 2),
-                            MinY = Math.Round(obb.MinPoint.Y, 2),
-                            MaxX = Math.Round(obb.MaxPoint.X, 2),
-                            MaxY = Math.Round(obb.MaxPoint.Y, 2),
-                            Corners = new List<double[]>
-                            {
-                                new[] { Math.Round(obb.LowerLeft.X, 2), Math.Round(obb.LowerLeft.Y, 2) },
-                                new[] { Math.Round(obb.UpperLeft.X, 2), Math.Round(obb.UpperLeft.Y, 2) },
-                                new[] { Math.Round(obb.UpperRight.X, 2), Math.Round(obb.UpperRight.Y, 2) },
-                                new[] { Math.Round(obb.LowerRight.X, 2), Math.Round(obb.LowerRight.Y, 2) }
-                            }
-                        },
-                        ResolvedGeometry = new MarkResolvedGeometryInfo
-                        {
-                            Source = geometry.Source,
-                            IsReliable = geometry.IsReliable,
-                            Width = Math.Round(geometry.Width, 2),
-                            Height = Math.Round(geometry.Height, 2),
-                            CenterX = Math.Round(geometry.CenterX, 2),
-                            CenterY = Math.Round(geometry.CenterY, 2),
-                            MinX = Math.Round(geometry.MinX, 2),
-                            MinY = Math.Round(geometry.MinY, 2),
-                            MaxX = Math.Round(geometry.MaxX, 2),
-                            MaxY = Math.Round(geometry.MaxY, 2),
-                            AngleDeg = Math.Round(geometry.AngleDeg, 2),
-                            AxisDx = Math.Round(geometry.AxisDx, 4),
-                            AxisDy = Math.Round(geometry.AxisDy, 4),
-                            Corners = geometry.Corners
-                                .Select(c => new[] { Math.Round(c[0], 2), Math.Round(c[1], 2) })
-                                .ToList()
-                        },
-                        ArrowHead = new MarkArrowheadInfo
-                        {
-                            Type = mark.Attributes.ArrowHead.Head.ToString(),
-                            Position = mark.Attributes.ArrowHead.ArrowPosition.ToString(),
-                            Height = Math.Round(mark.Attributes.ArrowHead.Height, 2),
-                            Width = Math.Round(mark.Attributes.ArrowHead.Width, 2)
-                        }
+                        RotationAngle = markContext?.RotationAngle ?? Math.Round(mark.Attributes.RotationAngle, 2),
+                        TextAlignment = markContext?.TextAlignment ?? mark.Attributes.TextAlignment.ToString(),
+                        ObjectAlignedBoundingBox = CreateObjectAlignedBoundingBoxInfo(obb),
+                        ResolvedGeometry = markContext?.Geometry != null
+                            ? CreateResolvedGeometryInfo(markContext.Geometry)
+                            : CreateResolvedGeometryInfo(MarkGeometryHelper.Build(mark, _model, vid)),
+                        ArrowHead = CreateArrowHeadInfo(mark)
                     };
 
-                    if (mark.Placing is BaseLinePlacing baseLinePlacing)
+                    if (markContext?.Axis != null)
+                    {
+                        info.Axis = CreateAxisInfo(markContext.Axis);
+                    }
+                    else if (mark.Placing is BaseLinePlacing baseLinePlacing)
                     {
                         var axisDx = baseLinePlacing.EndPoint.X - baseLinePlacing.StartPoint.X;
                         var axisDy = baseLinePlacing.EndPoint.Y - baseLinePlacing.StartPoint.Y;
@@ -134,21 +105,39 @@ public sealed partial class TeklaDrawingMarkApi
                         };
                     }
 
-                    var related = mark.GetRelatedObjects();
-                    while (related.MoveNext())
+                    if (markContext?.ModelId != null)
                     {
-                        if (related.Current is Tekla.Structures.Drawing.ModelObject mo)
+                        info.ModelId = markContext.ModelId;
+                    }
+                    else
+                    {
+                        var related = mark.GetRelatedObjects();
+                        while (related.MoveNext())
                         {
-                            info.ModelId = mo.ModelIdentifier.ID;
-                            break;
+                            if (related.Current is Tekla.Structures.Drawing.ModelObject mo)
+                            {
+                                info.ModelId = mo.ModelIdentifier.ID;
+                                break;
+                            }
                         }
                     }
 
-                    var contentEnum = mark.Attributes.Content.GetEnumerator();
-                    while (contentEnum.MoveNext())
+                    if (markContext?.Properties.Count > 0)
                     {
-                        if (contentEnum.Current is PropertyElement prop)
-                            info.Properties.Add(new MarkPropertyValue { Name = prop.Name, Value = prop.Value });
+                        info.Properties.AddRange(markContext.Properties.Select(static property => new MarkPropertyValue
+                        {
+                            Name = property.Name,
+                            Value = property.Value,
+                        }));
+                    }
+                    else
+                    {
+                        var contentEnum = mark.Attributes.Content.GetEnumerator();
+                        while (contentEnum.MoveNext())
+                        {
+                            if (contentEnum.Current is PropertyElement prop)
+                                info.Properties.Add(new MarkPropertyValue { Name = prop.Name, Value = prop.Value });
+                        }
                     }
 
                     var children = mark.GetObjects();
@@ -226,4 +215,116 @@ public sealed partial class TeklaDrawingMarkApi
     internal static bool ShouldSkipOverlapComparison(DrawingMarkInfo mark)
         => (mark.ResolvedGeometry?.Width ?? 0) < 0.1
            && (mark.ResolvedGeometry?.Height ?? 0) < 0.1;
+
+    internal static MarkObjectAlignedBoundingBoxInfo CreateObjectAlignedBoundingBoxInfo(RectangleBoundingBox obb)
+    {
+        return new MarkObjectAlignedBoundingBoxInfo
+        {
+            Width = Math.Round(obb.Width, 2),
+            Height = Math.Round(obb.Height, 2),
+            AngleToAxis = Math.Round(obb.AngleToAxis, 2),
+            CenterX = Math.Round((obb.MinPoint.X + obb.MaxPoint.X) / 2.0, 2),
+            CenterY = Math.Round((obb.MinPoint.Y + obb.MaxPoint.Y) / 2.0, 2),
+            MinX = Math.Round(obb.MinPoint.X, 2),
+            MinY = Math.Round(obb.MinPoint.Y, 2),
+            MaxX = Math.Round(obb.MaxPoint.X, 2),
+            MaxY = Math.Round(obb.MaxPoint.Y, 2),
+            Corners = new List<double[]>
+            {
+                new[] { Math.Round(obb.LowerLeft.X, 2), Math.Round(obb.LowerLeft.Y, 2) },
+                new[] { Math.Round(obb.UpperLeft.X, 2), Math.Round(obb.UpperLeft.Y, 2) },
+                new[] { Math.Round(obb.UpperRight.X, 2), Math.Round(obb.UpperRight.Y, 2) },
+                new[] { Math.Round(obb.LowerRight.X, 2), Math.Round(obb.LowerRight.Y, 2) }
+            }
+        };
+    }
+
+    internal static MarkResolvedGeometryInfo CreateResolvedGeometryInfo(MarkGeometryInfo geometry)
+    {
+        return new MarkResolvedGeometryInfo
+        {
+            Source = geometry.Source,
+            IsReliable = geometry.IsReliable,
+            Width = Math.Round(geometry.Width, 2),
+            Height = Math.Round(geometry.Height, 2),
+            CenterX = Math.Round(geometry.CenterX, 2),
+            CenterY = Math.Round(geometry.CenterY, 2),
+            MinX = Math.Round(geometry.MinX, 2),
+            MinY = Math.Round(geometry.MinY, 2),
+            MaxX = Math.Round(geometry.MaxX, 2),
+            MaxY = Math.Round(geometry.MaxY, 2),
+            AngleDeg = Math.Round(geometry.AngleDeg, 2),
+            AxisDx = Math.Round(geometry.AxisDx, 4),
+            AxisDy = Math.Round(geometry.AxisDy, 4),
+            Corners = geometry.Corners
+                .Select(c => new[] { Math.Round(c[0], 2), Math.Round(c[1], 2) })
+                .ToList()
+        };
+    }
+
+    internal static MarkResolvedGeometryInfo CreateResolvedGeometryInfo(MarkGeometryContext geometry)
+    {
+        var axisDx = 0.0;
+        var axisDy = 0.0;
+        var angleDeg = 0.0;
+        if (geometry.Corners.Count >= 2)
+        {
+            var dx = geometry.Corners[1].X - geometry.Corners[0].X;
+            var dy = geometry.Corners[1].Y - geometry.Corners[0].Y;
+            var length = Math.Sqrt((dx * dx) + (dy * dy));
+            if (length >= 0.001)
+            {
+                axisDx = dx / length;
+                axisDy = dy / length;
+                angleDeg = Math.Atan2(axisDy, axisDx) * (180.0 / Math.PI);
+            }
+        }
+
+        return new MarkResolvedGeometryInfo
+        {
+            Source = geometry.Source,
+            IsReliable = geometry.IsReliable,
+            Width = Math.Round(geometry.Width, 2),
+            Height = Math.Round(geometry.Height, 2),
+            CenterX = Math.Round(geometry.Center?.X ?? 0.0, 2),
+            CenterY = Math.Round(geometry.Center?.Y ?? 0.0, 2),
+            MinX = Math.Round(geometry.Bounds?.MinX ?? 0.0, 2),
+            MinY = Math.Round(geometry.Bounds?.MinY ?? 0.0, 2),
+            MaxX = Math.Round(geometry.Bounds?.MaxX ?? 0.0, 2),
+            MaxY = Math.Round(geometry.Bounds?.MaxY ?? 0.0, 2),
+            AngleDeg = Math.Round(angleDeg, 2),
+            AxisDx = Math.Round(axisDx, 4),
+            AxisDy = Math.Round(axisDy, 4),
+            Corners = geometry.Corners
+                .Select(c => new[] { Math.Round(c.X, 2), Math.Round(c.Y, 2) })
+                .ToList()
+        };
+    }
+
+    internal static MarkAxisInfo CreateAxisInfo(MarkAxisContext axis)
+    {
+        return new MarkAxisInfo
+        {
+            StartX = Math.Round(axis.Start?.X ?? 0.0, 2),
+            StartY = Math.Round(axis.Start?.Y ?? 0.0, 2),
+            EndX = Math.Round(axis.End?.X ?? 0.0, 2),
+            EndY = Math.Round(axis.End?.Y ?? 0.0, 2),
+            Dx = Math.Round(axis.Direction?.X ?? 0.0, 4),
+            Dy = Math.Round(axis.Direction?.Y ?? 0.0, 4),
+            Length = Math.Round(axis.Length, 2),
+            AngleDeg = Math.Round(axis.AngleDeg, 2),
+            IsReliable = axis.IsReliable
+        };
+    }
+
+    internal static MarkArrowheadInfo CreateArrowHeadInfo(Mark mark)
+    {
+        return new MarkArrowheadInfo
+        {
+            Type = mark.Attributes.ArrowHead.Head.ToString(),
+            Position = mark.Attributes.ArrowHead.ArrowPosition.ToString(),
+            Height = Math.Round(mark.Attributes.ArrowHead.Height, 2),
+            Width = Math.Round(mark.Attributes.ArrowHead.Width, 2)
+        };
+    }
 }
--- src/TeklaMcpServer.Tests/TeklaDrawingMarkApiQueryTests.cs
+++ src/TeklaMcpServer.Tests/TeklaDrawingMarkApiQueryTests.cs
@@ -46,6 +46,63 @@ public sealed class TeklaDrawingMarkApiQueryTests
         Assert.False(shouldSkip);
     }
 
+    [Fact]
+    public void CreateResolvedGeometryInfo_FromMarkGeometryContext_NormalizesAxisFromCorners()
+    {
+        var geometry = new MarkGeometryContext
+        {
+            Bounds = TeklaDrawingDimensionsApi.CreateBoundsInfo(10, 20, 30, 40),
+            Center = new DrawingPointInfo { X = 20, Y = 30 },
+            Width = 20,
+            Height = 10,
+            Source = "Context",
+            IsReliable = true,
+        };
+        geometry.Corners.Add(new DrawingPointInfo { X = 10, Y = 20, Order = 0 });
+        geometry.Corners.Add(new DrawingPointInfo { X = 30, Y = 20, Order = 1 });
+        geometry.Corners.Add(new DrawingPointInfo { X = 30, Y = 40, Order = 2 });
+        geometry.Corners.Add(new DrawingPointInfo { X = 10, Y = 40, Order = 3 });
+
+        var info = TeklaDrawingMarkApi.CreateResolvedGeometryInfo(geometry);
+
+        Assert.Equal("Context", info.Source);
+        Assert.True(info.IsReliable);
+        Assert.Equal(20, info.Width);
+        Assert.Equal(10, info.Height);
+        Assert.Equal(20, info.CenterX);
+        Assert.Equal(30, info.CenterY);
+        Assert.Equal(0, info.AngleDeg);
+        Assert.Equal(1, info.AxisDx);
+        Assert.Equal(0, info.AxisDy);
+        Assert.Equal(4, info.Corners.Count);
+    }
+
+    [Fact]
+    public void CreateAxisInfo_MapsContextFieldsWithExpectedRounding()
+    {
+        var axis = new MarkAxisContext
+        {
+            Start = new DrawingPointInfo { X = 10.123, Y = 20.456 },
+            End = new DrawingPointInfo { X = 30.789, Y = 40.012 },
+            Direction = new DrawingVectorInfo { X = 0.707106, Y = 0.707106 },
+            Length = 15.678,
+            AngleDeg = 45.123,
+            IsReliable = true,
+        };
+
+        var info = TeklaDrawingMarkApi.CreateAxisInfo(axis);
+
+        Assert.Equal(10.12, info.StartX);
+        Assert.Equal(20.46, info.StartY);
+        Assert.Equal(30.79, info.EndX);
+        Assert.Equal(40.01, info.EndY);
+        Assert.Equal(0.7071, info.Dx);
+        Assert.Equal(0.7071, info.Dy);
+        Assert.Equal(15.68, info.Length);
+        Assert.Equal(45.12, info.AngleDeg);
+        Assert.True(info.IsReliable);
+    }
+
     private static DrawingMarkInfo CreateMark(int id, double minX, double minY, double maxX, double maxY)
     {
         return new DrawingMarkInfo
